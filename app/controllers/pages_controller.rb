60a6ef48e186c9cacf389587bc28243496ad50dc
Julian Harrington <jrayharrington@gmail.com>
2020-12-30 08:57:59 +1100

    Created form and searchable results

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,18 @@
 class PagesController < ApplicationController
   def home
+    @date = Date.today.strftime("%B #{Date.today.day.ordinalize}")
   end
 
   def about
   end
 
   def contact
+    search = params[:member]
+    # raise
+    @members = %w[Julian Helena Bulut Greg Elle]
+
+    if search.present?
+      @members = @members.select { |name| name.start_with?(search) }
+    end
   end
 end
